6f14dd02aabc304ab3167d2e10fea7442f5c8500
Parin Mangal <22PMangal@heckgrammar.co.uk>
2025-04-04 10:15:05 +0100

    commit

--- BlankCSDotNet8/Program.cs
+++ BlankCSDotNet8/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            
+
+
+            DateTime current = DateTime.Now;
+            Console.WriteLine("The current date and time is : "); Console.WriteLine(current);
+            DateTime target = new DateTime(2025, 4, 6, 15, 15, 0);
+            Console.WriteLine("The target date and time is : "); Console.WriteLine(target);
+            TimeSpan difference = target - current;
+            if (difference.Seconds < 0)
+            {
+                Console.WriteLine("Spring break has started");
+            }else
+            {
+                if (difference.Days > 0)
+                {
+                    int daysleft = difference.Days;
+                    Console.WriteLine(" There is " + difference.Days + " Days, " + (difference.Hours /*+ daysleft * 24*/) + " hours, " + difference.Minutes + " minutes and " + difference.Seconds + " seconds left until the target time is reached");
+                }
+                else
+                {
+                    Console.WriteLine("There is " + difference.Hours + " hours, " + difference.Minutes + " minutes and " + difference.Seconds + " seconds left");
+                }
+            }
+           
+
         }
     }
-}
+}         
